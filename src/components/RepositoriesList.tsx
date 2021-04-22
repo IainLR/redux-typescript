a16fa3475c64551981b1aeb2fd24ece7693c7f2f
IainLR <iainrobertzon@gmail.com>
2021-04-22 14:09:12 -0500

    creating the useAction hook

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,23 @@
+import { useState } from 'react'
+// import { useDispatch } from 'react-redux'
+// import { actionCreators } from '../state'
+import { useActions } from '../hooks/useActions'
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('')
+  //   const dispatch = useDispatch()
+  const { searchRepositories } = useActions()
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // dispatch(actionCreators.searchRepositories(term))
+    searchRepositories(term)
+  }
+
   return (
     <div>
-      <form action=''>
-        <input type='text' />
+      <form onSubmit={handleSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
